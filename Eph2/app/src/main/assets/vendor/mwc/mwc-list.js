14b44d6343aac76272b3a03c3107eb62ce23414f
mattyperrott <104394280+mattyperrott@users.noreply.github.com>
2025-05-12 13:44:18 +0700

    Add Eph2/app/src/main/assets/vendor/mwc/mwc-list.js 1747032257815

--- Eph2/app/src/main/assets/vendor/mwc/mwc-list.js
+++ Eph2/app/src/main/assets/vendor/mwc/mwc-list.js
@@ -1,1 +1,20 @@
-Redirecting to /@material/mwc-list@0.27.0/mwc-list.js
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { __decorate } from "tslib";
+// Style preference for leading underscores.
+// tslint:disable:strip-private-property-underscore
+import { customElement } from 'lit/decorators.js';
+import { ListBase } from './mwc-list-base.js';
+import { styles } from './mwc-list.css.js';
+export { createSetFromIndex, isEventMulti, isIndexSet } from './mwc-list-foundation.js';
+let List = class List extends ListBase {
+};
+List.styles = [styles];
+List = __decorate([
+    customElement('mwc-list')
+], List);
+export { List };
+//# sourceMappingURL=mwc-list.js.map
