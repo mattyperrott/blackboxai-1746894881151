609442c4d72eb2738b8f644f7f37b0490a7d131c
mattyperrott <104394280+mattyperrott@users.noreply.github.com>
2025-05-12 13:44:07 +0700

    Add Eph2/app/src/main/assets/vendor/mwc/mwc-circular-progress.js 1747032246971

--- Eph2/app/src/main/assets/vendor/mwc/mwc-circular-progress.js
+++ Eph2/app/src/main/assets/vendor/mwc/mwc-circular-progress.js
@@ -1,1 +1,20 @@
-Redirecting to /@material/mwc-circular-progress@0.27.0/mwc-circular-progress.js
+/**
+ * @license
+ * Copyright 2018 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { __decorate } from "tslib";
+// Style preference for leading underscores.
+// tslint:disable:strip-private-property-underscore
+import { customElement } from 'lit/decorators.js';
+import { CircularProgressBase } from './mwc-circular-progress-base.js';
+import { styles } from './mwc-circular-progress.css.js';
+/** @soyCompatible */
+let CircularProgress = class CircularProgress extends CircularProgressBase {
+};
+CircularProgress.styles = [styles];
+CircularProgress = __decorate([
+    customElement('mwc-circular-progress')
+], CircularProgress);
+export { CircularProgress };
+//# sourceMappingURL=mwc-circular-progress.js.map
