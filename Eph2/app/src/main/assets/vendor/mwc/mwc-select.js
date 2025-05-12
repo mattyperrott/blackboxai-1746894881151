5514f005ac40b033622e42d217b605dd0b8ffb85
mattyperrott <104394280+mattyperrott@users.noreply.github.com>
2025-05-12 13:44:04 +0700

    Add Eph2/app/src/main/assets/vendor/mwc/mwc-select.js 1747032243969

--- Eph2/app/src/main/assets/vendor/mwc/mwc-select.js
+++ Eph2/app/src/main/assets/vendor/mwc/mwc-select.js
@@ -1,1 +1,19 @@
-Redirecting to /@material/mwc-select@0.27.0/mwc-select.js
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { __decorate } from "tslib";
+// Style preference for leading underscores.
+// tslint:disable:strip-private-property-underscore
+import { customElement } from 'lit/decorators.js';
+import { SelectBase } from './mwc-select-base.js';
+import { styles } from './mwc-select.css.js';
+let Select = class Select extends SelectBase {
+};
+Select.styles = [styles];
+Select = __decorate([
+    customElement('mwc-select')
+], Select);
+export { Select };
+//# sourceMappingURL=mwc-select.js.map
