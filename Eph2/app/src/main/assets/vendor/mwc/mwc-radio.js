0aefab0d744557f0a72664ab56f50e0d4cebc87e
mattyperrott <104394280+mattyperrott@users.noreply.github.com>
2025-05-12 13:44:13 +0700

    Add Eph2/app/src/main/assets/vendor/mwc/mwc-radio.js 1747032252740

--- Eph2/app/src/main/assets/vendor/mwc/mwc-radio.js
+++ Eph2/app/src/main/assets/vendor/mwc/mwc-radio.js
@@ -1,1 +1,20 @@
-Redirecting to /@material/mwc-radio@0.27.0/mwc-radio.js
+/**
+ * @license
+ * Copyright 2018 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { __decorate } from "tslib";
+// Style preference for leading underscores.
+// tslint:disable:strip-private-property-underscore
+import { customElement } from 'lit/decorators.js';
+import { RadioBase } from './mwc-radio-base.js';
+import { styles } from './mwc-radio.css.js';
+export { SingleSelectionController } from './single-selection-controller.js';
+let Radio = class Radio extends RadioBase {
+};
+Radio.styles = [styles];
+Radio = __decorate([
+    customElement('mwc-radio')
+], Radio);
+export { Radio };
+//# sourceMappingURL=mwc-radio.js.map
