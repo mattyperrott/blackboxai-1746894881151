869669c1883b7968ea9098b8f4f97218c0352d20
mattyperrott <104394280+mattyperrott@users.noreply.github.com>
2025-05-12 13:44:14 +0700

    Add Eph2/app/src/main/assets/vendor/mwc/mwc-fab.js 1747032254313

--- Eph2/app/src/main/assets/vendor/mwc/mwc-fab.js
+++ Eph2/app/src/main/assets/vendor/mwc/mwc-fab.js
@@ -1,1 +1,20 @@
-Redirecting to /@material/mwc-fab@0.27.0/mwc-fab.js
+/**
+ * @license
+ * Copyright 2018 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { __decorate } from "tslib";
+// Style preference for leading underscores.
+// tslint:disable:strip-private-property-underscore
+import { customElement } from 'lit/decorators.js';
+import { FabBase } from './mwc-fab-base.js';
+import { styles } from './mwc-fab.css.js';
+/** @soyCompatible */
+let Fab = class Fab extends FabBase {
+};
+Fab.styles = [styles];
+Fab = __decorate([
+    customElement('mwc-fab')
+], Fab);
+export { Fab };
+//# sourceMappingURL=mwc-fab.js.map
