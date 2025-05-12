5925a59db4d6aae7b7092da8c7654f40f79ca864
mattyperrott <104394280+mattyperrott@users.noreply.github.com>
2025-05-12 13:44:09 +0700

    Add Eph2/app/src/main/assets/vendor/mwc/mwc-top-app-bar-fixed.js 1747032248424

--- Eph2/app/src/main/assets/vendor/mwc/mwc-top-app-bar-fixed.js
+++ Eph2/app/src/main/assets/vendor/mwc/mwc-top-app-bar-fixed.js
@@ -1,1 +1,19 @@
-Redirecting to /@material/mwc-top-app-bar-fixed@0.27.0/mwc-top-app-bar-fixed.js
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { __decorate } from "tslib";
+// Style preference for leading underscores.
+// tslint:disable:strip-private-property-underscore
+import { customElement } from 'lit/decorators.js';
+import { styles } from '@material/mwc-top-app-bar/mwc-top-app-bar.css.js';
+import { TopAppBarFixedBase } from './mwc-top-app-bar-fixed-base.js';
+let TopAppBarFixed = class TopAppBarFixed extends TopAppBarFixedBase {
+};
+TopAppBarFixed.styles = [styles];
+TopAppBarFixed = __decorate([
+    customElement('mwc-top-app-bar-fixed')
+], TopAppBarFixed);
+export { TopAppBarFixed };
+//# sourceMappingURL=mwc-top-app-bar-fixed.js.map
