82b2fb93f301fec6c6cea869535e7850e9b1d88e
mattyperrott <104394280+mattyperrott@users.noreply.github.com>
2025-05-12 13:44:10 +0700

    Add Eph2/app/src/main/assets/vendor/mwc/mwc-formfield.js 1747032249803

--- Eph2/app/src/main/assets/vendor/mwc/mwc-formfield.js
+++ Eph2/app/src/main/assets/vendor/mwc/mwc-formfield.js
@@ -1,1 +1,19 @@
-Redirecting to /@material/mwc-formfield@0.27.0/mwc-formfield.js
+/**
+ * @license
+ * Copyright 2018 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { __decorate } from "tslib";
+// Style preference for leading underscores.
+// tslint:disable:strip-private-property-underscore
+import { customElement } from 'lit/decorators.js';
+import { FormfieldBase } from './mwc-formfield-base.js';
+import { styles } from './mwc-formfield.css.js';
+let Formfield = class Formfield extends FormfieldBase {
+};
+Formfield.styles = [styles];
+Formfield = __decorate([
+    customElement('mwc-formfield')
+], Formfield);
+export { Formfield };
+//# sourceMappingURL=mwc-formfield.js.map
